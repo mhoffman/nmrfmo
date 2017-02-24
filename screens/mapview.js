bddfce3c0c113cddc8552673a969c47a84c6b762
hoffmannm <mjhoffmann@gmail.com>
2017-02-23 16:25:09 -0800

    Completed category icons

--- screens/mapview.js
+++ screens/mapview.js
@@ -127,21 +127,27 @@ class CategoryIcon extends React.Component {
     render(){
         var size = this.props.size || 18;
         if(this.props.category =='Arts'){ return <VectorIcons.Ionicons name='ios-color-palette' size={size}/>
-        } else if (this.props.category=='Community'){ return <VectorIcons.MaterialIcons name='location-city' size={size}/>
-        } else if (this.props.category=='Concerts'){ return <VectorIcons.FontAwesome name='hand-rock-o' size={size}/>
-        } else if (this.props.category=='Dance'){ return <VectorIcons.Entypo name='sound' size={size}/>
-        } else if (this.props.category== 'Books'){ return  <VectorIcons.Entypo name='book' size={size}/>
-        } else if (this.props.category== 'Educational'){ return  <VectorIcons.Entypo name='open-book' size={size}/>
-        } else if (this.props.category == 'Kids & Family'){ return <VectorIcons.MaterialIcons name='child-friendly' size={size}/>
-        } else if (this.props.category== 'Movies'){ return <VectorIcons.MaterialIcons name='local-movies' size={size}/>
-        } else if (this.props.category== 'Meetup'){ return <VectorIcons.FontAwesome name='meetup' size={size}/>
-        } else if (this.props.category == 'Music'){ return  <VectorIcons.MaterialIcons name='music-note' size={size}/>
-        } else if (this.props.category == 'Outdoors'){ return  <VectorIcons.MaterialIcons name='nature-people' size={size}/>
-        } else if (this.props.category == 'Health & Fitness'){ return <VectorIcons.MaterialIcons name='fitness-center' size={size}/>
+        } else if (this.props.category == 'Activism'){ return <VectorIcons.FontAwesome name='hand-rock-o' size={size}/>
+        } else if (this.props.category == 'Books'){ return  <VectorIcons.Entypo name='book' size={size}/>
+        } else if (this.props.category == 'Comedy'){ return <VectorIcons.Octicons name='smiley' size={size}/>
+        } else if (this.props.category == 'Community'){ return <VectorIcons.MaterialIcons name='location-city' size={size}/>
+        } else if (this.props.category == 'Concerts'){ return <VectorIcons.FontAwesome name='hand-peace-o' size={size}/>
+        } else if (this.props.category == 'Dance'){ return <VectorIcons.Entypo name='sound' size={size}/>
+        } else if (this.props.category == 'Educational'){ return  <VectorIcons.Entypo name='blackboard' size={size}/>
+        } else if (this.props.category == 'Festivals'){ return  <VectorIcons.Ionicons name='ios-beer' size={size}/>
         } else if (this.props.category == 'Food & Drink'){ return <VectorIcons.MaterialIcons name='local-dining' size={size}/>
-        } else if (this.props.category == 'Nightlife'){ return <VectorIcons.Ionicons name='ios-cloudy-night' size={size}/>
-        } else if (this.props.category == 'Religious'){ return <VectorIcons.Entypo name='moon' size={size}/>
+        } else if (this.props.category == 'Health & Fitness'){ return <VectorIcons.MaterialIcons name='fitness-center' size={size}/>
+        } else if (this.props.category == 'Kids & Family'){ return <VectorIcons.MaterialIcons name='child-friendly' size={size}/>
+        } else if (this.props.category == 'Meetup'){ return <VectorIcons.FontAwesome name='meetup' size={size}/>
+        } else if (this.props.category == 'Movies'){ return <VectorIcons.MaterialIcons name='local-movies' size={size}/>
         } else if (this.props.category == 'Museums & Attractions'){ return <VectorIcons.FontAwesome name='building' size={size}/>
+        } else if (this.props.category == 'Music'){ return  <VectorIcons.MaterialIcons name='music-note' size={size}/>
+        } else if (this.props.category == 'Shopping'){ return  <VectorIcons.MaterialIcons name='shopping-basket' size={size}/>
+        } else if (this.props.category == 'Nightlife'){ return <VectorIcons.Ionicons name='ios-cloudy-night' size={size}/>
+        } else if (this.props.category == 'Outdoors'){ return  <VectorIcons.MaterialIcons name='nature-people' size={size}/>
+        } else if (this.props.category == 'Religious'){ return <VectorIcons.Entypo name='moon' size={size}/>
+        } else if (this.props.category == 'Theater'){ return <VectorIcons.Entypo name='mask' size={size}/>
+        } else if (this.props.category == 'Sports'){ return  <VectorIcons.Ionicons name='ios-american-football-outline' size={size}/>
         } else { return <VectorIcons.MaterialIcons name='check-box-outline-blank' size={size}/>
         }
     }
