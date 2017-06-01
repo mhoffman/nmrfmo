1f06c2c8283de1cd0c8999e9ee8a57a35497c9e9
hoffmannm <mjhoffmann@gmail.com>
2017-06-01 14:35:47 -0700

    Simplified main.js

--- main.js
+++ main.js
@@ -1,38 +1,5 @@
 'use strict';
 import Exponent from 'expo';
-import 'expo';
 import MapView from './screens/mapview';
-import React, { Component } from 'react';
-import {
-    Animated,
-    AppRegistry,
-    BackAndroid,
-    Dimensions,
-    Image,
-    InteractionManager,
-    Navigator,
-    Platform,
-    PropTypes,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableHighlight,
-    TouchableOpacity,
-    View
-
-} from 'react-native';
-
-class SView extends Component {
-    render(){
-    return(
-            <View>
-            <Text style={{marginTop: 40, textAlign: 'center'}}>Nothing there.</Text>
-            </View>
-            )
-    }
-}
-
 
 Exponent.registerRootComponent(MapView);
-/*Exponent.registerRootComponent(SView);*/
