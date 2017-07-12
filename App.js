ad0fff993fe80acf05a8685cec3635ea614f732f
hoffmannm <mjhoffmann@gmail.com>
2017-07-12 08:32:05 -0700

    Implemented persistance of AppState using redux-persist

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
 'use strict';
 import Exponent from 'expo';
-import React from 'react';
+import React, { Component } from 'react';
+import ReactNative from 'react-native';
 
+import { AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import thunk from 'redux-thunk'
 
 import Navigator from './scenes/navigator';
 import reducers from './store/reducers'
 
-const store = createStore(reducers, applyMiddleware(thunk))
+/*const store = createStore(reducers,*/
+/*undefined,*/
+/*compose(*/
+/*applyMiddleware(thunk),*/
+/*autoRehydrate()*/
+/*)*/
+/*)*/
 
-const ReduxApp = () => (
-    <Provider store={store}>
-        <Navigator />
-        </Provider>
-)
+const store = compose(autoRehydrate())(createStore)(reducers)
+if (typeof self === 'object'){
+    persistStore(store, {storage: AsyncStorage}, () => {
+        console.log('restored')
+    })
+}
+
+class ReduxApp extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            rehydrated: false
+        }
+    }
+    componentWillMount(){
+        persistStore(store, {storage: AsyncStorage}, () =>{
+            this.setState({rehydrated: true})
+            console.log('restored')
+
+        })
+    }
+    render(){
+        if(!this.state.rehydrated){
+            return <ReactNative.Text>Loading ...</ReactNative.Text>
+        }else{
+            return (
+                <Provider store={store}>
+                <Navigator />
+                </Provider>
+            )
+        }
+    }
+}
 export default ReduxApp
